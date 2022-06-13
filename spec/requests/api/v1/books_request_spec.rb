244c89671d6d3b3f4b82e07acbb4fc061b923f4a
Susan Boyd <sueboyd922@gmail.com>
2022-06-13 12:21:25 -0400

    Test: test book/forecast api response

--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -9,6 +9,24 @@ RSpec.describe "Book/Weather API request" do
       get "/api/v1/book-search?location=#{search}&quantity=#{quantity}"
 
       expect(response).to be_successful
+      returned_response = JSON.parse(response.body, symbolize_names: true)
+      returned = returned_response[:data]
+
+      expect(returned).to have_key(:id)
+      expect(returned[:id]).to eq("null")
+      expect(returned).to have_key(:type)
+      expect(returned[:type]).to eq("books")
+      expect(returned[:attributes]).to have_key(:destination)
+      expect(returned[:attributes][:destination]).to eq(search)
+      expect(returned[:attributes][:forecast]).to be_a Hash
+      expect(returned[:attributes][:forecast]).to have_key(:summary)
+      expect(returned[:attributes][:forecast]).to have_key(:temperature)
+      expect(returned[:attributes][:books]).to be_an Array
+      expect(returned[:attributes][:books].count).to eq(quantity)
+      expect(returned[:attributes][:books].first).to be_a Hash
+      expect(returned[:attributes][:books].first).to have_key(:isbn)
+      expect(returned[:attributes][:books].first).to have_key(:title)
+      expect(returned[:attributes][:books].first).to have_key(:publisher)
     end
   end
 end

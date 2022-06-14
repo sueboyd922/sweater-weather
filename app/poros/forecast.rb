37db0c6262814df8d136f2a33e8242691563f1f6
Susan Boyd <sueboyd922@gmail.com>
2022-06-14 12:47:37 -0400

    Feat: full vs partial forecast for hourly

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,17 +1,22 @@
 class Forecast
   attr_reader :current_weather, :daily_weather, :hourly_weather
 
-  def initialize(data)
+  def initialize(data, span)
     @current_weather = CurrentWeather.new(data[:current])
     @daily_weather = set_daily_weather(data[:daily][0..4])
-    @hourly_weather = set_hourly_weather(data[:hourly][0..7])
+    @hourly_weather = set_hourly_weather(data[:hourly], span)
   end
 
   def set_daily_weather(days)
     days.map {|day| DailyWeather.new(day)}
   end
 
-  def set_hourly_weather(hours)
-    hours.map {|hour| HourlyWeather.new(hour)}
+  def set_hourly_weather(hours, span)
+    if span == "full"
+      hours.map {|hour| HourlyWeather.new(hour)}
+    else
+      hours[0..7].map {|hour| HourlyWeather.new(hour)}
+    end
   end
+
 end

197af5f8d9558ae273f1ecffda3c08bbf90e5ef4
Susan Boyd <sueboyd922@gmail.com>
2022-06-14 10:58:19 -0400

    Move module to models/concerns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
-# require './controllers/modules/response_helper.rb'
-
 class ApplicationController < ActionController::API
-  # include ResponseHelper
+  include ResponseHelper
 
   def set_user
     if User.exists?(email: params[:email])
@@ -9,17 +7,5 @@ class ApplicationController < ActionController::API
     else
       login_error
     end
-  end
-
-  def success(user, code)
-    render json: UserSerializer.send_user(user), status: code
-  end
-
-  def login_error
-    render json: {error: "Email or password is incorrect"}, status: 400
-  end
-
-  def creation_error(user, code)
-    render json: {error: user.errors.full_messages.to_sentence}, status: 400
   end
 end
